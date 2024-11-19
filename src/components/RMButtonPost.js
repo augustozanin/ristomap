4e2e85dc97918db58c2f38a80e9b64becded00cd
Augusto Zanin <augustozann@gmail.com>
2024-11-18 21:37:46 -0300

    Adicionado botão para adicionar postagens na Home

---
+++ src/components/RMButtonPost.js
@@ -0,0 +1,29 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import React from 'react'
+import { colors, fontSizes } from '../styles/styles';
+
+export default function RMButtonPost() {
+  return (
+      <TouchableOpacity style={styles.botao}>
+        <Text style={styles.botaoTexto}>+</Text>
+      </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+    botao: {
+      width: "16%",
+      height: "8%",
+      backgroundColor: '#ffff',
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: 50,
+      position: "absolute",
+      bottom: 20, // Distância da borda inferior
+      right: 20,  // Distância da borda direita
+    },
+    botaoTexto: {
+      fontSize: fontSizes.grande,
+      color: colors.vermelho,
+    },
+  });
