a27adf219e083a88efbb91b29295bf5bd791a36d
Augusto Zanin <augustozann@gmail.com>
2024-11-19 22:32:02 -0300

    Estruturação da página - inclusão do topBar - inclusão dos campos de texto

--- src/pages/CriaPost.js
+++ src/pages/CriaPost.js
@@ -1,13 +1,87 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Alert } from 'react-native'
 import { colors, fontSizes, globalStyles } from '../styles/styles';
-import React from 'react'
+import React, { useState } from 'react'
 import TopBar from '../components/TopBar';
+import RMTextInput from '../components/RMTextInput';
+import RMButton from '../components/RMButton';
+import { useAuth } from "../context/AuthContext";
+import { supabase } from '../services/supabase';
 
 export default function CriaPost({ navigation }) {
+  const { user } = useAuth();
+  const [titulo, setTitulo] = useState(null)
+  const [descricao, setDescricao] = useState(null)
+
+  const [erroTitulo, setErroTitulo] = useState(false)
+  const [erroDescricao, setErroDescricao] = useState(false)
+
+  const [loading, setLoading] = useState(false);
+
+  async function registraPost() {
+    if (loading) {
+      return;
+    }
+
+    setErroTitulo(false)
+    setErroDescricao(false)
+
+    if (!titulo) {
+      setErroTitulo(true)
+    }
+
+    if (!descricao) {
+      setErroDescricao(true)
+    }
+
+    if (!titulo || !descricao) {
+      return
+    }
+
+    setLoading(true);
+
+    try {    
+      console.log('Opa vamos tentar cadastrar um POST!!! dados a seguir ')
+      console.log(user.usuario,titulo,descricao)
+      // inserindo post no supabase
+      const { data, error } = await supabase.from('post').insert([
+        { user_username: user.usuario, title: titulo, description: descricao },
+      ]);
+
+      if (error) {
+        Alert.alert('Erro', error.message);
+      } else {
+        //ir pra home apos criar conta
+        setUser({ username: usuario, email });
+
+        Alert.alert('Sucesso', 'Post criado!');
+        navigation.replace('Home'); 
+      }
+
+    } catch (error) {
+      console.error('Erro inesperado:', error);
+      Alert.alert('Erro', 'Ocorreu um erro inesperado.');
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <TopBar navigation={navigation} />
-      <Text></Text>
+      <RMTextInput
+        value={titulo}
+        onChangeText={setTitulo}
+        placeholder="Titulo: "
+      />
+
+      <RMTextInput
+        value={descricao}
+        onChangeText={setDescricao}
+        placeholder="Descrição: "
+      />
+
+      <RMButton titulo="Postar" action={registraPost} />
+
     </View>
   )
 }
